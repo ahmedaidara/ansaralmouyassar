406ea02a1f2e883a516711552f4f11b7625fa993
ahmedaidara <ahmedsaidaidara05@gmail.com>
2025-07-16 23:35:52 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('ansar-almouyassar-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/main.css',
+        '/main.js',
+        '/assets/images/logo.png',
+        '/assets/videos/intro.mp4'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
