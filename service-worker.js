9ea5082bbeb9fabb3be0f4b4a1eee71c40337fe1
ahmedaidara <ahmedsaidaidara05@gmail.com>
2025-07-17 16:17:51 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,39 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('ansar-almouyassar-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/main.css',
+        '/main.js',
+        '/chatbotResponses.js',
+        '/assets/images/logo.png',
+        '/assets/images/default-photo.png',
+        '/assets/images/conference.jpg',
+        '/assets/images/chatbot-bg.jpg',
+        '/assets/videos/intro.mp4',
+        'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js',
+        'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js',
+        'https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', () => {
-  self.clients.claim();
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  const title = data.title || 'ANSAR ALMOUYASSAR';
+  const options = {
+    body: data.body,
+    icon: '/assets/images/logo.png'
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
 });
