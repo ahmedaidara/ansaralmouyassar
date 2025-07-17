ce89e28b7620a83f6ee198500b19f9f4b32b16cf
ahmedaidara <ahmedsaidaidara05@gmail.com>
2025-07-17 15:58:50 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,11 @@
-const CACHE_NAME = 'ansar-almouyassar-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/main.css',
-  '/main.js',
-  '/chatbotResponses.js',
-  '/manifest.json',
-  '/assets/images/logo.png',
-  '/assets/images/icon-192.png',
-  '/assets/images/icon-512.png',
-  '/assets/videos/intro.mp4',
-  '/assets/images/default-photo.png'
-];
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('activate', () => {
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
